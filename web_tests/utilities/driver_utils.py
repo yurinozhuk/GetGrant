47b1ad52d1b5779b9b4f3f763404f5c7ad472544
Yurii Zhuk <yuriisdb@gmail.com>
2023-12-17 16:15:00 +0200

    upd

--- web_tests/utilities/driver_utils.py
+++ web_tests/utilities/driver_utils.py
@@ -1,5 +1,7 @@
+import os
+
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.options import Options
 
 
 def get_driver(browser="chrome", headless=True):
@@ -11,12 +13,21 @@ def get_driver(browser="chrome", headless=True):
     :return: The WebDriver instance (e.g., ChromeDriver).
     :raises ValueError: If an unsupported browser is provided.
     """
+    chrome_options = Options()
+
+    if headless:
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("window-size=1200x600")
+
+        # Додавання опцій для зберігання знімків у вигляді артефактів
+        if "CI" in os.environ:
+            chrome_options.add_argument("--disable-gpu")
+            chrome_options.add_argument("--no-sandbox")
+
+    chrome_options.add_argument("--start-maximized")
+
+    # Ініціалізація WebDriver
     if browser.lower() == "chrome":
-        chrome_options = ChromeOptions()
-        if headless:
-            chrome_options.add_argument("--headless")
-            chrome_options.add_argument("window-size=1200x600")
-        chrome_options.add_argument("--start-maximized")
         return webdriver.Chrome(options=chrome_options)
     else:
         raise ValueError(f"Unsupported browser '{browser}'. Please choose 'chrome'.")
