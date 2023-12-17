17b7b119cbd0caf9a6ce2df35bcb3bbc76d57eea
Yurii Zhuk <yuriisdb@gmail.com>
2023-12-17 11:13:17 +0200

    Ubuntu upd

--- web_tests/utilities/driver_utils.py
+++ web_tests/utilities/driver_utils.py
@@ -2,18 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 
-def get_driver(browser="chrome"):
+def get_driver(browser="chrome", headless=True):
     """
     Get a WebDriver instance for the specified browser.
 
     :param browser: The browser to use (options: "chrome").
+    :param headless: Whether to run the browser in headless mode.
     :return: The WebDriver instance (e.g., ChromeDriver).
     :raises ValueError: If an unsupported browser is provided.
     """
     if browser.lower() == "chrome":
         chrome_options = ChromeOptions()
+        if headless:
+            chrome_options.add_argument("--headless")
         chrome_options.add_argument("--start-maximized")
-        # chrome_options.add_argument("--headless")
         return webdriver.Chrome(options=chrome_options)
     else:
         raise ValueError(f"Unsupported browser '{browser}'. Please choose 'chrome'.")
