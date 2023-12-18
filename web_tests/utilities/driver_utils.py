317d6c730cb76fd4d7370025107b3e5f7c8953c3
Yurii Zhuk <yuriisdb@gmail.com>
2023-12-18 12:09:02 +0200

    just one more....

--- web_tests/utilities/driver_utils.py
+++ web_tests/utilities/driver_utils.py
@@ -15,6 +15,7 @@ def get_driver(browser="chrome", headless=True):
         chrome_options = ChromeOptions()
         if headless:
             chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--no-sandbox")
             chrome_options.add_argument("window-size=1920x1080")
         chrome_options.add_argument("--start-maximized")
         return webdriver.Chrome(options=chrome_options)
