2fa6f0bad38886b8c4f5e89d9f6ae1535136e269
Yurii Zhuk <yuriisdb@gmail.com>
2023-12-28 15:57:54 +0200

    upd

--- web_tests/utilities/driver_utils.py
+++ web_tests/utilities/driver_utils.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 
-def get_driver(browser="chrome", headless=True):
+def get_driver(browser="chrome", headless=False):
     """
     Get a WebDriver instance for the specified browser.
 
@@ -12,14 +12,21 @@ def get_driver(browser="chrome", headless=True):
     :return: The WebDriver instance (e.g., ChromeDriver).
     :raises ValueError: If an unsupported browser is provided.
     """
+    # if browser.lower() == "chrome":
+    #     chrome_options = ChromeOptions()
+    #     if headless:
+    #         chrome_options.add_argument("--headless")
+    #     chrome_options.add_argument("--start-maximized")
+    #     return webdriver.Chrome(options=chrome_options)
+
     if browser.lower() == "chrome":
         chrome_options = ChromeOptions()
         # chrome_options = FirefoxOptions()
         chrome_options.add_argument("--start-maximized")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--headless")
+
         return webdriver.Remote(
-            command_executor='http://selenium-hub:4444/wd/hub',
+            command_executor='http:selenium-hub:4444/wd/hub',
+            # command_executor='http://127.0.0.1:4444/wd/hub',
             options=chrome_options)
 
         if headless:
