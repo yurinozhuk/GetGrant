37878efd8a0d4d299cfa3f5996e304d057bc10dc
Yurii Zhuk <yuriisdb@gmail.com>
2024-01-03 13:27:36 +0200

    upd driver_utils.py

--- web_tests/utilities/driver_utils.py
+++ web_tests/utilities/driver_utils.py
@@ -9,6 +9,7 @@ def get_driver(browser="chrome", headless=False, remote=True):
     """
     Get a WebDriver instance for the specified browser.
 
+    :param remote:
     :param browser: The browser to use (options: "chrome").
     :param headless: Whether to run the browser in headless mode.
     :return: The WebDriver instance (e.g., ChromeDriver).
@@ -25,17 +26,13 @@ def get_driver(browser="chrome", headless=False, remote=True):
         chrome_options = ChromeOptions()
         # chrome_options = FirefoxOptions()
         chrome_options.add_argument("--start-maximized")
-
-        return webdriver.Remote(
-            command_executor=f'http://{Config.SELENIUM_GRID_URL_}:{Config.SELENIUM_GRID_PORT}/wd/hub',
-            options=chrome_options)
-
         if headless:
             chrome_options.add_argument("--headless")
             chrome_options.add_argument("--no-sandbox")
             chrome_options.add_argument("window-size=1920x1080")
-        chrome_options.add_argument("--start-maximized")
-        return webdriver.Chrome(options=chrome_options)
+        return webdriver.Remote(
+            command_executor=f'http://{Config.SELENIUM_GRID_URL_}:{Config.SELENIUM_GRID_PORT}/wd/hub',
+            options=chrome_options)
     else:
         raise ValueError(f"Unsupported browser '{browser}'. Please choose 'chrome'.")
 
